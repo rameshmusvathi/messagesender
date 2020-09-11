e4ed7c0de3de00fb1148de8adeea68d3c1968702
rameshmusvathi <53107519+rameshmusvathi@users.noreply.github.com>
2020-09-11 17:41:48 +0800

    Add files via upload

---
+++ emailsender.js
@@ -0,0 +1,22 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+service: 'gmail',
+auth: {
+user:'rameshmusuvathi@gmail.com',
+pass:'lillylotus'
+}
+});
+var mailOptions = {
+  from:'rameshmusuvathi@gmail.com',
+  to:'mails2kirupa@gmail.com',
+  subject:'Sending Email using Node.js in html format',
+  html: '<h1>My first html email for Node js</h1><p>That was easy</p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
