c84742985277a8c7a3291a2e0fbb56b4fb5efa70
rameshmusvathi <53107519+rameshmusvathi@users.noreply.github.com>
2020-11-12 23:52:36 +0800

    Update messagingapp.js

--- messagingapp.js
+++ messagingapp.js
@@ -25,8 +25,8 @@ const getWeatherData = async () => {
 
   const main = async () => {
   const weatherData = await getWeatherData()
-  const weatherMessage = `The weather in ${weatherData.name}: ${weatherData.weather[0].description}. Current temperature is ${weatherData.main.temp} degree celsius, with a low temp of ${weatherData.main.temp_min} degree celsius and high of ${weatherData.main.temp_max} degree celsius.`
-  console.log(weatherMessage)
+  console.log(`The weather in ${weatherData.name}: ${weatherData.weather[0].description}. Current temperature is ${weatherData.main.temp} degree celsius, with a low temp of ${weatherData.main.temp_min} degree celsius and high of ${weatherData.main.temp_max} degree celsius.`)
+  
     }
 
 main()
