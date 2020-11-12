cc574d96a9c450fe780d1fa7c3951661fa8cbc9c
rameshmusvathi <53107519+rameshmusvathi@users.noreply.github.com>
2020-11-13 01:14:30 +0800

    Update messagingapp.js

--- messagingapp.js
+++ messagingapp.js
@@ -22,7 +22,7 @@ fetch(
             const forecast = data.weather[0].description;
             const temperature = data.main.temp;
             const name = data.name;
-            bot.sendMessage(process.env.TELEGRAMCHATID, "Some one push/pull file in messagesender repo")
+            bot.sendMessage(process.env.TELEGRAMCHATID, " Messengrjs file push/pull in messagesender repo")
             console.log(`Today's forecast for ${name}: ${forecast}`);
             console.log(`Current temperature in ${name} is ${temperature}°C `);
         })
